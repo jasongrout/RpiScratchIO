a0a8995fe46c83819a77191101cc7a1b945d84eb
William H. Bell <W.Bell@cern.ch>
2014-01-26 21:50:45 +0100

    Cleanup

--- t.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from GpioSvc import *
-g = GpioSvc()
-print "setOutput=%d" % g.setOutput(23)
-print "setInput=%d" % g.setInput(23)
-g.release(23)
-g.write(23,True)
-print "read(23)=%d" % g.read(23)
-
