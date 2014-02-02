18742c499ce7efc62fafcbfc2c6436e47d2cabef
William H. Bell <W.Bell@cern.ch>
2014-02-02 22:08:36 +0100

    Adding changes

--- setup.py
+++ setup.py
@@ -6,12 +6,14 @@ setup(
     author='W. H. Bell',
     author_email='whbqcd1@gmail.com',
     packages=['RpiScratchIO'],
-    scripts=['bin/RpiScratchIO.py'],
+    scripts=['bin/RpiScratchIO'],
     url='http://pypi.python.org/pypi/RpiScratchIO/',
     license='LICENSE.txt',
     description='Easy expansion of Raspberry Pi I/O within Scratch',
     long_description=open('README.txt').read(),
     install_requires=[
         "scratchpi == 0.1.0",
+        "spidev == 2.0",
+        "RPi.GPIO >= 0.5.4",
     ],
 )
