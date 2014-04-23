6c71c04df87736794ee68dc9b902716604221b1e
William H. Bell <W.Bell@cern.ch>
2014-04-23 08:57:35 +0200

    Bug fixes

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ classifiers = ['Development Status :: 3 - Alpha',
 
 setup(
     name='RpiScratchIO',
-    version='0.1.6',
+    version='0.1.7',
     author='W. H. Bell',
     author_email='whbqcd1@gmail.com',
     packages=['RpiScratchIO'],
